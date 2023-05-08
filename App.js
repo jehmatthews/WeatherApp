0f3d981c178376f1227947d6708e2dbebc164210
jehmatthews <jehmatthews30@gmail.com>
2023-05-08 14:22:37 -0700

    create useGetWeather hook to use in App.js and refactor App.js

--- App.js
+++ App.js
@@ -4,58 +4,23 @@ import { NavigationContainer } from "@react-navigation/native"
 import Tabs from "./src/components/Tabs"
 import * as Location from 'expo-location'
 import { WEATHER_API_KEY} from '@env';
-
-//api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+import { useGetWeather } from "./src/hooks/useGetWeather"
 
 const App = () => {
-  const [loading, setLoading] = useState(true)
-  const [error, setError] = useState(null)
-  const [weather, setWeather] = useState([])
-  const [lat, setLat] = useState([])
-  const [lon, setLon] = useState([])
+  const [loading, error, weather] = useGetWeather()
+  console.log(loading, error, weather)
 
-  const fetchWeatherData = async() => {
-    try {
-      const res = await fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`)
-      const data = await response.json()
-      setWeather(data)
-    } catch (error) {
-      setError('Could not fetch weather')
-    } finally {
-      setLoading(false)
-    }
-
-  }
-  useEffect(() => {
-    (async() => {
-      let { status } = await Location.requestForegroundPermissionsAsync()
-      if (status !== 'granted') {
-        setError('permission to access location was denied')
-        return
-      }
-      let location = await Location.getCurrentPositionAsync({})
-      setLat(location.coords.latitude)
-      setLon(location.coords.longitude)
-      await fetchWeatherData()
-    })()
-  }, [lat, lon])
-
-  if (weather) {
-    console.log(weather)
-  }
-
-  if (loading) {
+  if (weather && weather.list) {
     return (
-      <View style={styles.container}>
-        <ActivityIndicator size={'large'} color={'blue'} />
-      </View>
+      <NavigationContainer>
+        <Tabs weather={weather} />
+      </NavigationContainer>
     )
   }
-
   return (
-    <NavigationContainer>
-      <Tabs />
-    </NavigationContainer>
+    <View style={styles.container}>
+      <ActivityIndicator size={'large'} color={'blue'} />
+    </View>
   )
 }
 
