54b56a4d7ad426effafaf380bc841764c4b9de6f
jehmatthews <jehmatthews30@gmail.com>
2023-05-08 14:13:59 -0700

    set .env key from weather api and implement into fetWeatherData function. refactor useEffect to call location

--- App.js
+++ App.js
@@ -3,16 +3,29 @@ import { ActivityIndicator, View, StyleSheet } from "react-native"
 import { NavigationContainer } from "@react-navigation/native"
 import Tabs from "./src/components/Tabs"
 import * as Location from 'expo-location'
-import { TEST_KEY } from '@env';
+import { WEATHER_API_KEY} from '@env';
 
 //api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 
 const App = () => {
   const [loading, setLoading] = useState(true)
-  const [location, setLocation] = useState(null)
   const [error, setError] = useState(null)
-  console.log(TEST_KEY)
+  const [weather, setWeather] = useState([])
+  const [lat, setLat] = useState([])
+  const [lon, setLon] = useState([])
 
+  const fetchWeatherData = async() => {
+    try {
+      const res = await fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`)
+      const data = await response.json()
+      setWeather(data)
+    } catch (error) {
+      setError('Could not fetch weather')
+    } finally {
+      setLoading(false)
+    }
+
+  }
   useEffect(() => {
     (async() => {
       let { status } = await Location.requestForegroundPermissionsAsync()
@@ -21,12 +34,14 @@ const App = () => {
         return
       }
       let location = await Location.getCurrentPositionAsync({})
-      setLocation(location)
+      setLat(location.coords.latitude)
+      setLon(location.coords.longitude)
+      await fetchWeatherData()
     })()
-  }, [])
+  }, [lat, lon])
 
-  if (location) {
-    console.log(location)
+  if (weather) {
+    console.log(weather)
   }
 
   if (loading) {
