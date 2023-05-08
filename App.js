c2b5de524a3ad1c872595a50276866447563d9a6
jehmatthews <jehmatthews30@gmail.com>
2023-05-08 13:59:15 -0700

    set up a useEffect function to grant permission to location with expo-location

--- App.js
+++ App.js
@@ -1,9 +1,41 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+import { ActivityIndicator, View, StyleSheet } from "react-native"
 import { NavigationContainer } from "@react-navigation/native"
 import Tabs from "./src/components/Tabs"
+import * as Location from 'expo-location'
+import { TEST_KEY } from '@env';
 
+//api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 
 const App = () => {
+  const [loading, setLoading] = useState(true)
+  const [location, setLocation] = useState(null)
+  const [error, setError] = useState(null)
+  console.log(TEST_KEY)
+
+  useEffect(() => {
+    (async() => {
+      let { status } = await Location.requestForegroundPermissionsAsync()
+      if (status !== 'granted') {
+        setError('permission to access location was denied')
+        return
+      }
+      let location = await Location.getCurrentPositionAsync({})
+      setLocation(location)
+    })()
+  }, [])
+
+  if (location) {
+    console.log(location)
+  }
+
+  if (loading) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size={'large'} color={'blue'} />
+      </View>
+    )
+  }
 
   return (
     <NavigationContainer>
@@ -12,4 +44,10 @@ const App = () => {
   )
 }
 
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    flex: 1
+  }
+})
 export default App
